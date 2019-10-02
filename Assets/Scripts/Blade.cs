6a1048791ada37bd506d1ff80f983eac5eb7167f
Manu <black_falcon00@hotmail.com>
2019-10-02 20:41:37 +0200

    Done

---
+++ Assets/Scripts/Blade.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+
+    public GameObject bladeTrailPrefab;
+    public float minCutttingVelocity = .001f;
+
+    bool isCutting = false;
+
+    Rigidbody2D rb;
+    CircleCollider2D circleCollider;
+    Camera cam;
+
+    GameObject currentBladeTrail;
+
+    Vector2 previousPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCutting();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopCutting();
+        }
+
+        if (isCutting)
+        {
+            UpdateCut();
+        }
+    }
+
+    void StartCutting()
+    {
+        isCutting = true;
+        currentBladeTrail = Instantiate(bladeTrailPrefab, transform);
+        circleCollider.enabled = true;
+        previousPos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void StopCutting()
+    {
+        isCutting = false;
+        currentBladeTrail.transform.SetParent(null);
+        Destroy(currentBladeTrail, 2);
+        circleCollider.enabled = false;
+    }
+
+    void UpdateCut()
+    {
+        Vector2 newPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        rb.position = newPos;
+
+        float velocity = (newPos - previousPos).magnitude * Time.deltaTime;
+        if (velocity > minCutttingVelocity)
+        {
+            circleCollider.enabled = true;
+        }
+        else
+        {
+            circleCollider.enabled = false;
+        }
+
+        previousPos = rb.position;
+    }
+}
---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject fruitSlicedPrefab;
+    public float startForce = 15f;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * startForce, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Blade")
+        {
+            Vector3 direction = (collision.transform.position - transform.position).normalized;
+
+            GameObject slicedFruit = Instantiate(fruitSlicedPrefab, transform.position, Quaternion.LookRotation(direction));
+            Destroy(gameObject);
+            Destroy(slicedFruit, 3f);
+        }
+    }
+}
